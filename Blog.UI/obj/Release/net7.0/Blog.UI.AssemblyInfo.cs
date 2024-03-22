b2dbe74c9ba2952401ea3cbec11797759fdc785c
hieund20 <hieund898@gmail.com>
2024-03-23 01:02:31 +0700

    modify date format

--- Blog.UI/obj/Release/net7.0/Blog.UI.AssemblyInfo.cs
+++ Blog.UI/obj/Release/net7.0/Blog.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dde023da3abe87800162233d84332f77ce4d754e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f902c799fbac1b49a619662d1d4917dd2bc5985")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
