e6adbd551ebb4ccb313ab0a474435712ad4b26a7
hieund20 <hieund898@gmail.com>
2024-03-25 17:00:26 +0700

    add logo

--- Blog/obj/Debug/net7.0/Blog.API.AssemblyInfo.cs
+++ Blog/obj/Debug/net7.0/Blog.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f902c799fbac1b49a619662d1d4917dd2bc5985")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2dbe74c9ba2952401ea3cbec11797759fdc785c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
